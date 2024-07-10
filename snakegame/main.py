08dd685ca059d9e2b4e4752b8b9b567b45e88363
Austin Harrison <austin.harrisonn@yahoo.com>
2024-07-10 19:17:52 -0400

    Attempting to fix import errors

--- snakegame/main.py
+++ snakegame/main.py
@@ -1,17 +1,26 @@
-import pygame
 import sys
 import random
+import pygame
 
 pygame.init()
 
 SW, SH = 800, 800
 
-FONT = pygame.font.Font("font.ttf", 100)
+BLOCK_SIZE = 50
+FONT = pygame.font.Font("font.ttf", BLOCK_SIZE*2)
 
 screen = pygame.display.set_mode((800, 800))
 pygame.display.set_caption("Snake")
 clock = pygame.time.Clock()
 
+def draw_grid():
+    for x in range(0, SW, BLOCK_SIZE):
+        for y in range(0, SH, BLOCK_SIZE):
+            rect = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
+            pygame.draw.rect(screen, "#3c3c3b", rect, 1)
+
+
+running = True
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -20,3 +29,4 @@ while True:
 
     pygame.display.update()
     clock.tick(10)
+    
