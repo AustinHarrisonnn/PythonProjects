bf8a0c0fdb7dc89da7525d63d594761509a442c2
Austin Harrison <austin.harrisonn@yahoo.com>
2024-07-10 19:36:00 -0400

    Initialized snake class to display snake and mark changes within the game when cetrain events occur

--- snakegame/main.py
+++ snakegame/main.py
@@ -7,11 +7,44 @@ pygame.init()
 SW, SH = 800, 800
 
 BLOCK_SIZE = 50
-FONT = pygame.font.Font("font.ttf", BLOCK_SIZE*2)
+FONT = pygame.font.Font(None, BLOCK_SIZE*2)
 
 screen = pygame.display.set_mode((800, 800))
 pygame.display.set_caption("Snake")
 clock = pygame.time.Clock()
+
+class Snake:
+    def __init__(self):
+        self.x, self.y = BLOCK_SIZE, BLOCK_SIZE
+        self.xdir = 1
+        self.ydir = 0
+        self.head = pygame.Rect(self.x, self.y, BLOCK_SIZE, BLOCK_SIZE)
+        self.body = [pygame.Rect(self.x-BLOCK_SIZE, self.y, BLOCK_SIZE, BLOCK_SIZE)]
+        self.dead = False
+
+    def update(self):
+        global apple
+
+        for square in self.body:
+            if self.head.x == square.x and self.head.y == square.y:
+                self.dead = True
+            if self.head.x not in range(0, SW) or self.head.y not in range(0, SH):
+                self.dead = True
+
+        if self.dead:
+            self.x, self.y = BLOCK_SIZE, BLOCK_SIZE
+            self.head = pygame.Rect(self.x, self.y, BLOCK_SIZE, BLOCK_SIZE)
+            self.body = [pygame.Rect(self.x-BLOCK_SIZE, self.y, BLOCK_SIZE, BLOCK_SIZE)]
+            self.xdir = 1
+            self.ydir = 0
+            self.dead = False
+            apple = Apple()
+
+        self.body.append(self.head)
+        for i in range(len(self.body)-1):
+            self.body[i].x, self.body[i].y = self.body[i+1].x, self.body[i+1].y
+        self.head.x += self.xdir * BLOCK_SIZE
+        self.body.remove(self.head)
 
 def draw_grid():
     for x in range(0, SW, BLOCK_SIZE):
@@ -19,11 +52,13 @@ def draw_grid():
             rect = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
             pygame.draw.rect(screen, "#3c3c3b", rect, 1)
 
+draw_grid()
 
 running = True
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            pygame.display.quit()
             pygame.quit()
             sys.exit()
 
